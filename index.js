1eb2a7b0ae884895e6e8b92a82a5bda09b04cc80
Ravin01 <ravinganapathi@gmail.com>
2023-09-22 09:48:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 // Import the express
-const express = require('express');
+const express = require("express");
 const app = express();
 // Port
-const port = 5000;
+const port = 5001;
 // Accessing file system
-const fs = require('fs');
-// Accessing data object
-const date = new Date();
-const data = date.toISOString();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const day = date.getDay();
-const hours = date.getHours();
-const mins = date.getMinutes();
-const sec = date.getSeconds();
-// Variable for storing file name
-const fileName = `./apiFolder/Date${year}-${month}-${day},time${hours}-${mins}-${sec}.txt`;
-// Writing the files with current date and time
-fs.writeFile(fileName, data, (err) => {
-    if (err) throw err;
-    console.log("complete writing file")
-})
-// Api end point which will be creating the files
-app.get('/', function (req, res) {
-    res.send(`Completed Writing File, FileName : ${fileName}, Content : ${data}`)
-})
-// empty array
-const retrieveData = [];
-// Read the folder and push the every file into the array
-fs.readdir("./apiFolder", (err, file) => {
-    file.forEach((file) => {
-        fs.readFile(`./apiFolder/${file}`, "utf-8", (err, data) => {
-            if (err) throw err;
-            return retrieveData.push(data);
-        })
-    });
-});
-// Api end point for accessing the file content.
-app.get('/api', function (req, res) {
-    res.send(retrieveData);
-})
+const fs = require("fs");
 
+
+// Api end point which will be creating the files
+app.get("/", function (req, res) {
+  let date = new Date();
+  let data = date.valueOf().toString();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let  day = date.getDay();
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let sec = date.getSeconds();
+  // Variable for storing file name
+  const fileName = `Date_${year}-${month}-${day},time_${hours}-${mins}-${sec}.txt`;
+  // Creating file
+  fs.writeFile(`./apiFolder/${fileName}`, data, (err) => {
+    if (err) throw err;
+    console.log("completed writing file : ", fileName);
+  });
+  res.send(
+    `Completed Writing File, File name : ${fileName}, Content of the file is : ${data}, urlPath : /api to get all the files`
+  );
+});
+
+
+// Api end point for accessing the files.
+app.get("/file", (req, res)=> {
+  fs.readdir("./apiFolder", (err, file) => {
+    res.send(file)
+  });
+});
 
 
 // server running port number
 app.listen(port, () => {
-    console.log("server is running in ", port);
-})
+  console.log("server is running in ", port);
+});
